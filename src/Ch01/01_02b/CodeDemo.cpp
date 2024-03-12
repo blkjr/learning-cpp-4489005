413bd2e84f75db203a713e55f3f640dc2f65b975
blkjr <85901351+blkjr@users.noreply.github.com>
2024-03-12 17:22:11 -0500

    Update CodeDemo.cpp

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,10 @@
 // Learning C++ 
 // Exercise 01_02
 // Hello World, by Eduardo Corpeño 
+// Modified 03/12/2024 5:14 PM - blkjr
+
+#include <iostream> //Pre-porcessor directive to include <iostream> standard library 
+
+int main(){
+  std::out << "High there!"
+}
